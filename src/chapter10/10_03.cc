37448429efba354313d5e15a8bb2d14a5cdef4ff
sunnyswag <hhq126152@gmail.com>
2025-04-28 23:51:29 +0800

    fix 10_03 accumulate start index

--- src/chapter10/10_03.cc
+++ src/chapter10/10_03.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <numeric>
 
@@ -8,7 +7,7 @@ using std::cout; using std::endl; using std::vector; using std::accumulate;
 int main()
 {
     vector<int> v{1, 4, 5, 6, 10};
-    cout << accumulate(v.cend(), v.cend(), 0) << endl;
+    cout << accumulate(v.cbegin(), v.cend(), 0) << endl;
 
     return 0;
 }
